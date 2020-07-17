75f35b0c1f9df99b4a50a1127a8af110ff7d0e21
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2020-07-17 13:02:41 +0300

    Добавляет загрузку объявлений, убирает их генерацию

---
+++ js/backend.js
@@ -0,0 +1,53 @@
+'use strict';
+
+(function () {
+  var TIMEOUT_IN_MS = 10000;
+  var StatusCode = {
+    OK: 200
+  };
+  var Urls = {
+    URL_TO: 'https://javascript.pages.academy/keksobooking',
+    URL_FROM: 'https://javascript.pages.academy/keksobooking/data'
+  };
+
+  var prepareXhr = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    return xhr;
+  };
+
+  var load = function (onLoad, onError) {
+    var xhr = prepareXhr(onLoad, onError);
+
+    xhr.open('GET', Urls.URL_FROM);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad, onError) {
+    var xhr = prepareXhr(onLoad, onError);
+
+    xhr.open('POST', Urls.URL_TO);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
