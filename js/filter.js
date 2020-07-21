58a0651b1c4fc7adca4522f2c28d727ca8c44d69
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2020-07-21 22:14:11 +0300

    Исправляет функцию фильтрации

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,28 @@
 'use strict';
 
 (function () {
+  var ALL_OPTIONS_VALUE = 'any';
   var filters = document.querySelector('.map__filters');
   var housingTypeFilter = filters.querySelector('#housing-type');
-  var currentHousingType = 'any';
+  var currentHousingType = ALL_OPTIONS_VALUE;
 
   var applyFilter = function (adv, filterField, filterValue) {
-    var filteredAdv = adv;
+    var filteredAdv = [];
 
-    if (filterValue !== 'any') {
-      filteredAdv = adv.filter(function (it) {
-        return it.offer[filterField] === filterValue;
-      });
+    for (var i = 0; i < adv.length; i++) {
+      if (equals(adv[i].offer[filterField], filterValue) || filterValue === ALL_OPTIONS_VALUE) {
+        filteredAdv.push(adv[i]);
+      }
+      if (filteredAdv.length === window.data.MAX_ADV_COUNT) {
+        break;
+      }
     }
 
     return filteredAdv;
+  };
+
+  var equals = function (field, value) {
+    return field === value;
   };
 
   window.filter = {
